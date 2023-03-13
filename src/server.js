63b8e44ac96cd5136879a9c5bb9555e2ff8ec35f
RGoedert <renato.goedert@gmail.com>
2023-03-13 21:45:51 +0000

    Skeleton

---
+++ src/server.js
@@ -0,0 +1,57 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import Hapi from "@hapi/hapi";
+import Vision from "@hapi/vision";
+import Handlebars from "handlebars";
+import path from "path";
+import { fileURLToPath } from "url";
+import Cookie from "@hapi/cookie";
+import { webRoutes } from "./web-routes.js";
+import { db } from "./models/db.js";
+import { accountsController } from "./controllers/accounts-controller.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+async function init() {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+  });
+
+  await server.register(Vision);
+  await server.register(Cookie);
+
+  server.auth.strategy("session", "cookie", {
+    cookie: {
+      name: "playtime",
+      password: "secretpasswordnotrevealedtoanyone",
+      isSecure: false,
+    },
+    redirectTo: "/",
+    validate: accountsController.validate,
+  });
+  server.auth.default("session");
+
+  server.views({
+    engines: {
+      hbs: Handlebars,
+    },
+    relativeTo: __dirname,
+    path: "./views",
+    layoutPath: "./views/layouts",
+    partialsPath: "./views/partials",
+    layout: true,
+    isCached: false,
+  });
+  db.init();
+  server.route(webRoutes);
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+}
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
