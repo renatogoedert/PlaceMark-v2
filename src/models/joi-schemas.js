99196a50f37cfa39a86becfa55018f36b2ee7a29
RGoedert <renato.goedert@gmail.com>
2023-03-14 18:49:24 +0000

    User, placemark and place schemas

--- src/models/joi-schemas.js
+++ src/models/joi-schemas.js
@@ -1,24 +1,53 @@
 import Joi from "joi";
 
-export const UserSpec = {
-  firstName: Joi.string().required(),
-  lastName: Joi.string().required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
-};
+export const IdSpec = Joi.alternatives().try(Joi.string(), Joi.object()).description("a valid ID");
 
-export const UserCredentialsSpec = {
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
-};
+export const UserCredentialsSpec = Joi.object()
+  .keys({
+    email: Joi.string().email().example("homer@simpson.com").required(),
+    password: Joi.string().example("secret").required(),
+  })
+  .label("UserCredentials");
 
-export const PlaceSpec = {
-  name: Joi.string().required(),
-  lat: Joi.number().allow("").optional(),
-  lon: Joi.number().allow("").optional(),
-  des: Joi.string().required(),
-};
+export const UserSpec = UserCredentialsSpec.keys({
+  firstName: Joi.string().example("Homer").required(),
+  lastName: Joi.string().example("Simpson").required(),
+}).label("UserDetails");
 
-export const PlacemarkSpec = {
-  name: Joi.string().required(),
-};
+export const UserSpecPlus = UserSpec.keys({
+  _id: IdSpec,
+  __v: Joi.number(),
+}).label("UserDetailsPlus");
+
+export const UserArray = Joi.array().items(UserSpecPlus).label("UserArray");
+
+export const PlaceSpec = Joi.object()
+.keys({
+  name: Joi.string().required().example("Dublin"),
+  lat: Joi.number().allow("").required().example(53.34),
+  lon: Joi.number().allow("").required().example(-6.26),
+  des: Joi.string().optional().example("Dublin, capital of the Republic of Ireland"),
+})
+.label("Place");
+
+export const PlaceSpecPlus = PlaceSpec.keys({
+  _id: IdSpec,
+  __v: Joi.number(),
+}).label("PlacePlus");
+
+export const PlaceArraySpec = Joi.array().items(PlaceSpecPlus).label("PlaceArray");
+
+export const PlacemarkSpec = Joi.object()
+.keys({
+  name: Joi.string().required().example("cities"),
+  userid: IdSpec,
+  places: PlaceArraySpec,
+})
+.label("Placemark");
+
+export const PlacemarkSpecPlus = PlacemarkSpec.keys({
+  _id: IdSpec,
+  __v: Joi.number(),
+}).label("PlacemarkPlus");
+
+export const PlacemarkArraySpec = Joi.array().items(PlacemarkSpecPlus).label("PlacemarkArray");
