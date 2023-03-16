28570c22793127548c269f7ace2ae93c69b06148
Renato Francisco Goedert <renato.goedert@gmail.com>
2023-03-16 00:28:11 +0000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -18,6 +18,12 @@ import { validate } from "./api/jwt-utils.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const result = dotenv.config();
+if (result.error) {
+  console.log(result.error.message);
+  process.exit(1);
+}
+
 const swaggerOptions = {
   info: {
     title: "Placemark API",
